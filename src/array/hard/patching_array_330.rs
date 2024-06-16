f5ba9570891fd006ebc550eae5a2f7d1d1a1b10b
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-06-16 12:52:25 +0500

    min_patches

--- src/array/hard/patching_array_330.rs
+++ src/array/hard/patching_array_330.rs
@@ -2,20 +2,21 @@ pub struct Solution;
 
 impl Solution {
     pub fn min_patches(nums: Vec<i32>, n: i32) -> i32 {
-        let nums = nums.into_iter().map(|x| x as i64).collect::<Vec<_>>();
+        let (mut index, mut count, mut value_need) = (0usize, 0, 1i64);
 
-        let mut index = 0;
-        let mut count = 0;
-        let mut value_need = 1_i64;
         while value_need <= n as i64 {
-            if index < nums.len() && value_need >= nums[index] {
-                value_need += nums[index];
-                index += 1;
-            } else {
-                value_need += value_need;
-                count += 1;
+            if index < nums.len() {
+                let current = nums[index] as i64;
+                if value_need >= current {
+                    value_need += current;
+                    index += 1;
+                    continue;
+                }
             }
+            value_need += value_need;
+            count += 1;
         }
+
         count
     }
 }

755484427a9b577fd208b0e293e26a9c136a584e
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-12-06 22:09:14 +0500

    task_2554

--- src/array/medium/task_2554.rs
+++ src/array/medium/task_2554.rs
@@ -3,25 +3,21 @@ use std::collections::HashSet;
 pub struct Solution;
 
 impl Solution {
-    pub fn max_count(mut banned: Vec<i32>, n: i32, max_sum: i32) -> i32 {
-        let mut arr = Vec::new();
-
-        for i in 1..=n {
-            arr.push(i);
-        }
-        banned = banned.into_iter().collect::<HashSet<i32>>().into_iter().collect::<Vec<i32>>();
-        banned.sort_unstable();
-
-        let mut banned_idx = 0;
-        let mut current_sum = 0;
+    // beat 100%
+    pub fn max_count(banned: Vec<i32>, n: i32, max_sum: i32) -> i32 {
         let mut total = 0;
+        let mut sum = 0;
+        let max_possible_size = (*banned.iter().max().unwrap() + 1).max(n + 1) as usize;
 
-        for i in 0..n as usize {
-            if banned_idx < banned.len() && arr[i] == banned[banned_idx] {
-                banned_idx += 1;
-            } else {
-                if current_sum + arr[i] <= max_sum {
-                    current_sum += arr[i];
+        let mut banned_calc = vec![0; max_possible_size];
+
+        for &v in banned.iter() {
+            banned_calc[v as usize] += 1;
+        }
+        for i in 1..=n {
+            if banned_calc[i as usize] == 0 {
+                if sum + i <= max_sum {
+                    sum += i;
                     total += 1;
                 } else {
                     return total;

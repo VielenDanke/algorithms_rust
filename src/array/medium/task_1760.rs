5d464584d5d51fb1781b7c641e3968f216dd0fc2
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-12-09 09:21:51 +0500

    task_1760

--- src/array/medium/task_1760.rs
+++ src/array/medium/task_1760.rs
@@ -18,25 +18,39 @@ impl Solution {
         left
     }
 
-    pub fn minimum_size_brute_force_second(nums: Vec<i32>, max_operations: i32) -> i32 {
-        fn can_divide(nums: &Vec<i32>, max_balls: i32, max_operations: i32) -> bool {
-            let mut ops = 0;
-            for &n in nums {
-                ops += (n as f64 / max_balls as f64).ceil() as i32 - 1;
-                if ops > max_operations {
-                    return false;
-                }
-            }
-            true
-        }
+    pub fn minimum_size_brute_force_binary_search(nums: Vec<i32>, max_operations: i32) -> i32 {
+        let (mut left, mut right) = (1, *nums.iter().max().unwrap());
 
+        while left < right {
+            let mid = left + ((right - left) / 2);
+            if Self::can_divide(&nums, mid, max_operations) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        left
+    }
+
+    pub fn minimum_size_brute_force_second(nums: Vec<i32>, max_operations: i32) -> i32 {
         for i in 1..=(*nums.iter().max().unwrap()) {
-            if can_divide(&nums, i, max_operations) {
+            if Self::can_divide(&nums, i, max_operations) {
                 return i;
             }
         }
 
         -1
+    }
+
+    fn can_divide(nums: &Vec<i32>, max_balls: i32, max_operations: i32) -> bool {
+        let mut ops = 0;
+        for &n in nums {
+            ops += (n as f64 / max_balls as f64).ceil() as i32 - 1;
+            if ops > max_operations {
+                return false;
+            }
+        }
+        true
     }
 
     pub fn minimum_size_brute_force(mut nums: Vec<i32>, max_operations: i32) -> i32 {

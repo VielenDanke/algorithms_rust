5e6215d669bf04f06666d97df0dd961637dada1d
Vladislav Dankevich <vielendanke1991@gmail.com>
2025-12-20 12:33:14 +0500

    working on

--- src/strings/easy/task_944.rs
+++ src/strings/easy/task_944.rs
@@ -4,14 +4,14 @@ impl Solution {
     pub fn min_deletion_size(strs: Vec<String>) -> i32 {
         let mut result = 0;
 
-        'l: for i in 0..strs[0].len() {
+        for i in 0..strs[0].len() {
             let mut prev = 0;
             for str in strs.iter() {
                 let str_bytes = str.as_bytes();
 
                 if str_bytes[i] < prev {
                     result += 1;
-                    continue 'l;
+                    break;
                 }
                 prev = str_bytes[i];
             }
